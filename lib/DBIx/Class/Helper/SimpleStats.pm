7ab931daaa1d6bc4a643cfa4fab7d80a6ea119b9
Robert Rothenberg <rrwo@cpan.org>
2019-04-02 18:56:17 +0100

    Add prototypes

---
+++ lib/DBIx/Class/Helper/SimpleStats.pm
@@ -0,0 +1,146 @@
+package DBIx::Class::Helper::SimpleStats;
+
+# ABSTRACT: Simple grouping and aggregate functions for DBIx::Class
+
+use v5.10.1;
+
+use strict;
+use warnings;
+
+use base qw( DBIx::Class );
+
+use Carp;
+use List::Util qw/ uniqstr /;
+use Ref::Util qw/ is_plain_hashref is_ref /;
+
+# RECOMMEND PREREQ: Ref::Util::XS
+
+use namespace::autoclean;
+
+our $VERSION = 'v0.1.0';
+
+=head1 SYNOPSIS
+
+In a resultset class:
+
+  package My::Schema::ResultSet::Foo;
+
+  use base 'DBIx::Class::ResultSet';
+
+  __PACKAGE__->load_components('Helper::SimpleStats');
+
+  ...
+
+In code
+
+  my @stats = $rs->simple_stats( 'name' )->all;
+
+is roughly equivalent to
+
+  my @stats = $rs->search_rs(
+    undef,
+    {
+      select   => [qw/ name /, { count => 'name' }],
+      as       => [qw/ name name_count /],
+      group_by => [qw/ name /],
+      order_by => [qw/ name /],
+    }
+  )->all;
+
+=head1 DESCRIPTION
+
+This is a simple helper method for L<DBIx::Class> resultsets to run
+simple aggregate queries.
+
+The simplest usage is to pass a single column name, and obtain a count
+of rows for each value of that column.
+
+=cut
+
+sub simple_stats {
+    my ( $self, @args ) = @_;
+
+    croak "No columns" unless @args;
+
+    my @cols;
+    my @funcs;
+
+    my $me = $self->current_source_alias;
+
+    my $alias = sub {
+      my $ident = shift;
+      $ident = "$me.$ident" unless $ident =~ /^(\w+)\./;
+      return $ident;
+    };
+
+    foreach my $arg (@args) {
+
+        if ( is_ref($arg) ) {
+
+            if ( is_plain_hashref($arg) ) {
+
+                my $as = delete $arg->{'-as'};
+                my ( $func, $col ) = each %{$arg};
+
+                $as //= "${col}_${func}";
+
+                push @cols, $alias->($col);
+
+                push @funcs, { $func => $alias->($col), -as => $as };
+
+            }
+            else {
+
+                croak "Unsupported reference type: " . ref($arg);
+
+            }
+
+        }
+        else {
+
+            push @cols, $alias->($arg);
+
+        }
+
+    }
+
+    unless (@funcs) {
+
+        my $func = "count";
+        my $col  = $cols[0];
+        $col =~ s/^\w+\.// ;
+
+        push @funcs, { $func => $alias->($col), -as => "${col}_${func}" };
+
+    }
+
+    my @names = map { delete $_->{'-as'} } @funcs;
+
+    my @group = uniqstr @cols;
+
+    return $self->search(
+        undef,
+        {
+            group_by => \@group,
+            select  => [ @group, @funcs ],
+            as      => [ @group, @names ],
+            order_by => \@group,
+        }
+    );
+
+}
+
+
+=head1 SEE ALSO
+
+L<DBIx::Class>
+
+=head1 append:AUTHOR
+
+The initial development of this module was sponsored by Science Photo
+Library L<https://www.sciencephoto.com>.
+
+=cut
+
+
+1;
